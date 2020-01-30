14ed08d19007fca8653d3b758a8a3ca1aa2cd826
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-01-30 15:47:39 +0800

    feat: 🎸 add index + upload pages

---
+++ pages/upload.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+const uploadPageContent = <p>This is the upload page</p>;
+
+export default function Upload() {
+    return <Layout content={uploadPageContent} />;
+}
